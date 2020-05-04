df404535daf69a0bc30289da67a391a989cc7b16
Misha Syrbu <mihaisyrbu91@gmail.com>
2020-05-04 22:45:16 +0300

    fix node start

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan(':date[iso] - notice: :remote-addr ":method :url" :req[X-Request-Id]', { immediate: true }));
 app.use(morgan(':date[iso] - notice: :remote-addr ":method :url" :status :res[content-length] :res[X-Request-Id]'));
 
+app.get('*', function(request, response) {
+    response.send('Hello World!')
+});
+
 app.get('/', function(request, response) {
     response.send('Hello World!')
 });
